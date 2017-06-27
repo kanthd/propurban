d6e9e798089977b0d9caa38947443fcf0fab84bc
kanthd10 <kanth.d10@gmail.com>
2017-06-27 18:49:15 +0530

    added overflow slider, action buttons

--- js/property-list.js
+++ js/property-list.js
@@ -1,1 +1,14 @@
-
+$('.btn-show-more').click( function (e) {
+    e.stopPropagation();
+    var $curr = $(".btn-show-more");
+    var $prev = $curr.parent().parent();
+    if ($prev.hasClass('active')) {
+      if ($prev.find('tbody').hasClass('collapse-content')) {
+          $prev.find('tbody').removeClass('collapse-content');
+          $curr.children().removeClass('fa-angle-up').addClass('fa-angle-down');
+      } else {
+          $prev.find('tbody').addClass('collapse-content');
+          $curr.children().removeClass('fa-angle-down').addClass('fa-angle-up');
+      }
+    }
+});
